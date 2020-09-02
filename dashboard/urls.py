a481a48170d54b0a7aac6dfb67ffd06b2eeae309
Onwughara-CK <kelechicollins.93@gmail.com>
2020-09-02 22:43:34 +0100

    add story list view and story list template

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -8,4 +8,12 @@ app_name = 'dash'
 
 urlpatterns = [
     path('', views.DashView.as_view(), name='dashboard'),
+
+    # story url
+    path('story/', views.StoryListView.as_view(), name='story-list'),
+    # path('story/<int:pk>/', views.StoryDetailView.as_view(), name='story-detail'),
+    # path('story/<int:pk>/', views.StoryDeleteView.as_view(), name='story-delete'),
+    # path('story/<int:pk>/update/',
+    #      views.StoryUpdateView.as_view(), name='story-update'),
+    # path('story/create/', views.StoryCreateView.as_view(), name='story-create'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render
-from django.views import View
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.core.exceptions import PermissionDenied
+from django.views import generic, View
+from django.urls import reverse_lazy
+from django.contrib import messages
+
+from . import models
 
 
-class DashView(View):
+class DashView(LoginRequiredMixin, UserPassesTestMixin, View):
     def get(self, request, *args, **kwargs):
         return render(request, 'dashboard/dash.html')
+
+    def test_func(self):
+        user = self.request.user
+        if not user.teacher:
+            raise PermissionDenied
+        return True
+
+
+class StoryListView(LoginRequiredMixin, UserPassesTestMixin, generic.ListView):
+    model = models.Story
+    template_name = "dashboard/story_list.html"
+    context_object_name = 'stories'
+
+    def test_func(self):
+        user = self.request.user
+        if not user.teacher:
+            raise PermissionDenied
+        return True
