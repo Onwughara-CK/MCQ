a4f51db4d2c111e9ca454ab418ee03a99268fd8e
Onwughara-CK <kelechicollins.93@gmail.com>
2020-09-02 22:55:41 +0100

    add story list view and story list template

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Story, Question, Choice
 
-# Register your models here.
+
+class StoryAdmin(admin.ModelAdmin):
+    list_display = ('story_title',)
+    search_fields = ('story_title', 'story_text')
+    ordering = ('story_title',)
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text',)
+    search_fields = ('question_text',)
+    ordering = ('question_text',)
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('choice_text',)
+    search_fields = ('choice_text',)
+    ordering = ('choice_text',)
+
+
+admin.site.register(Story, StoryAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
