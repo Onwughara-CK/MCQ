25e61d156df608c41e1b801405a0cb428fa5b239
Onwughara-CK <kelechicollins.93@gmail.com>
2020-09-16 15:26:58 +0100

    write test for question model

--- dashboard/tests/test_models.py
+++ dashboard/tests/test_models.py
@@ -1,6 +1,7 @@
 from datetime import timedelta
 
 from django.test import TestCase, Client
+from django.db import models as django_models
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 
@@ -48,3 +49,45 @@ class QuizModelTest(TestCase):
     def test_get_update_url(self):
         self.assertEqual(self.quiz.get_update_url(),
                          f'/dashboard/quiz/{self.quiz.pk}/update/')
+
+
+class QuestionModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # set up unmodifiable test data
+        cls.quiz = models.Quiz.objects.create(
+            quiz_title='title 1',
+            quiz_text='text 1',
+            duration=timedelta(minutes=25)
+        )
+
+        cls.question = models.Question.objects.create(
+            question_text='text 1',
+            quiz=cls.quiz,
+        )
+
+    def test_question_text_label(self):
+        field_label = self.question._meta.get_field(
+            'question_text').verbose_name
+        self.assertEqual(field_label, 'question text')
+
+    def test_quiz_label(self):
+        field_label = self.question._meta.get_field('quiz').verbose_name
+        self.assertEqual(field_label, 'quiz')
+
+    def test_get_absolute_url(self):
+        self.assertEqual(self.question.get_absolute_url(),
+                         f'/dashboard/question/{self.question.pk}/')
+
+    def test_get_delete_url(self):
+        self.assertEqual(self.question.get_delete_url(),
+                         f'/dashboard/question/{self.question.pk}/delete/')
+
+    def test_get_update_url(self):
+        self.assertEqual(self.question.get_update_url(),
+                         f'/dashboard/question/{self.question.pk}/update/')
+
+    def test_question_foreign_key_relationship_with_quiz(self):
+        self.assertEqual(self.quiz.questions.count(), 1)
+        self.assertEqual(self.quiz.questions.first().pk, self.question.pk)
+        self.assertEqual(str(self.question.quiz), str(self.quiz))
