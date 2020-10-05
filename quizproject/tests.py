5962c6daaafe187e2ed0b779a710602ca3859192
Onwughara-CK <kelechicollins.93@gmail.com>
2020-10-05 12:39:15 +0100

    wrote test for quizproject

---
+++ quizproject/tests.py
@@ -0,0 +1,27 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import resolve, reverse
+from django.test import SimpleTestCase, TestCase
+
+from users import views as users_views
+from dashboard import views as dash_views
+from exam import views as exam_views
+
+
+
+class UrlTest(TestCase):
+    def test_home_url(self):
+        self.assertEqual(resolve('/').func.view_class,users_views.IndexView)
+        self.assertEqual(resolve('/').namespace,'users')
+
+    def test_dashboard_url(self):
+        self.assertEqual(resolve('/dashboard/').func.view_class,dash_views.DashView)
+        self.assertEqual(resolve('/dashboard/').namespace,'dash')
+
+    def test_exam_url(self):
+        self.assertEqual(resolve('/exam/').func.view_class,exam_views.ExamListView)
+        self.assertEqual(resolve('/exam/').namespace,'exam')
+    
+
+
+
